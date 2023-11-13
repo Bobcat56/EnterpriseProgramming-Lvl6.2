83b54a5f539e32954dad70f9ba4d7d856babedfc
Andrea Baldacchino <andrea.balds@gmail.com>
2023-11-13 09:42:41 +0100

    Controllers being defined

---
+++ HomeAssignment_Andrea_Baldacchino/Presentation/Controllers/TicketsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public IActionResult ListFlights()
+        {   
+            /* This is to return a web page which displays flights that can be booked
+                    a.) Fully booked flights CAN NOT be selected, BUT still displayed 
+                    b.) If Departure date is in the past, DO NOT DISPLAY
+            */
+
+            return View();
+        }
+
+        public IActionResult BookFlight()
+        {
+
+
+            return View();
+        }
+
+
+        public IActionResult ListTicketHistory()
+        {
+
+
+
+            return View();
+        }
+
+
+
+
+
+    }
+}
