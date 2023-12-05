a8d9b92698d83840b762c4ab76076870783691d0
Andrea Baldacchino <andrea.balds@gmail.com>
2023-12-05 21:11:29 +0100

    Flight and ticket models set up

---
+++ HomeAssignment_Andrea_Baldacchino/Domain/Models/Flight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    /* Flight.cs should contain the following
+     * Id, Rows, Columns, DepartureDate, ArrivalDate, CountryFrom, CountryTo, WholesalePrice, CommissionRate */
+    public class Flight
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public DateOnly DepartureDate { get; set; }
+        public DateOnly ArrivalDate { get; set; }
+        public string CountryFrom { get; set; }
+        public string CountryTo { get; set; }
+        public double WholeSalePrice { get; set; }
+        public double ComissionRate { get; set; }
+    
+    }
+}
---
+++ HomeAssignment_Andrea_Baldacchino/Domain/Models/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    /* Ticket.cs should contain the following
+     * Id, Row, Column, FlightIdFK, Passport, PricePaid, Cancelled */
+    public class Ticket
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        [ForeignKey(nameof(Flight))]
+        public int FlightIdFK { get; set; }
+        public Flight Flight { get; set; }
+        public string Passport { get; set; }
+        public double PricePaid { get; set; }
+        public Boolean Canelled { get; set; }
+
+    }
+}
